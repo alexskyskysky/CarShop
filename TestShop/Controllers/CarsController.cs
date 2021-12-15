303c0613981ad5ba990553cdd653f77a86fe4b62
skyskysky <undergrounder_fm@mail.ru>
2021-12-15 11:14:08 +0300

    test

--- TestShop/Controllers/CarsController.cs
+++ TestShop/Controllers/CarsController.cs
@@ -29,7 +29,6 @@ namespace Shop.Controllers {
                     cars = _allCars.Cars.Where(i => i.Category.categoryName == "Электромобили").OrderBy(i => i.id);
                     currCategory = "Электромобили";
                 }
-
                 if (string.Equals("Fuel", category, StringComparison.OrdinalIgnoreCase)) {
                     cars = _allCars.Cars.Where(i => i.Category.categoryName == "Классические автомобили")
                         .OrderBy(i => i.id);
